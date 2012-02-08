30251a3e204e78081d5795c6bdf2e4b2e291ddba
Silvan Wegmann <swegmann@narf.ch>
2012-02-08 21:16:32 +0100

    Add copyright headers

--- getch.py
+++ getch.py
@@ -1,3 +1,21 @@
+########################################################################
+#
+# This file is part of Timetracker.
+#
+# Timetracker is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Timertracker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Timetracker. If not, see <http://www.gnu.org/licenses/>.
+#
+########################################################################
 class _Getch:
     """Gets a single character from standard input.  Does not echo to the
 screen."""
